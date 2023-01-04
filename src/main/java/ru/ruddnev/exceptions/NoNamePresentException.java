7af1b1c1372600f6c5b04cc79e092d0ec40150cb
ruddnev <nikita-rudnev@ya,ru>
2023-01-04 20:45:48 +0300

    little text change

--- src/main/java/ru/ruddnev/exceptions/NoNamePresentException.java
+++ src/main/java/ru/ruddnev/exceptions/NoNamePresentException.java
@@ -2,6 +2,6 @@ package ru.ruddnev.exceptions;
 
 public class NoNamePresentException extends Exception {
     public NoNamePresentException() {
-        super("Извините, имя обязательно для выполнения программы!");
+        super("Извините, но имя обязательно для выполнения программы!");
     }
 }
